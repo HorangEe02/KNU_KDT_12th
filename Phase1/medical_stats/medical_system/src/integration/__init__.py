495541a7ca15be84048403e6ef0538463f047e7b
HorangEe02 <catlife9029@gmail.com>
2026-03-05 19:27:12 +0900

    Add Phase1: Phase1/medical_stats/medical_system/src/integration/__init__.py

---
+++ Phase1/medical_stats/medical_system/src/integration/__init__.py
@@ -0,0 +1,11 @@
+"""
+integration 패키지
+건강 체크 시스템과 환자 관리 시스템 연동
+
+Author: KDT12 Python Project
+Date: 2026-01-09
+"""
+
+from .integration_manager import IntegrationManager
+
+__all__ = ['IntegrationManager']
